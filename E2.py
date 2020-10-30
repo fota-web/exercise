a47fa2ddfedb6ca1001f0d1316e21e81d3160340
for ChainChain <67625993+fota-web@users.noreply.github.com>
2020-10-30 09:39:03 +0800

    Add files via upload

--- E2.py
+++ E2.py
@@ -6,11 +6,11 @@ A = pulp.LpVariable("internet",lowBound = 0)
 B = pulp.LpVariable("print",lowBound = 0)
 
 # Maximize
-ans += 3000*A + 900*B
+ans += 200000*A + 80000*B
 
 # Constraints
 ans += 3000*A + 900*B <= 75000
-ans += B - 5*A >= 0
+ans += B - 5*A <= 0
 ans += 5 >= A <= 15
 ans += A >= 0
 ans += B >= 0
